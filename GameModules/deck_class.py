fcc2b03c7266e4144ff1023c03796acdf512d862
Xhonklei <132895700+Xhonklei@users.noreply.github.com>
2025-04-29 16:41:18 +0900

    Update deck_class.py
    Adding a function to add played cards back to the bottom of the deck.

--- GameModules/deck_class.py
+++ GameModules/deck_class.py
@@ -12,27 +12,32 @@ class Deck:
 
     def __init__(self):
         # Create the deck with all cards combinations
-        self.all_card = []
+        self.all_cards = []
         for suit in suits:
             for rank in ranks:
-                self.all_card.append(Card(suit,rank))
+                self.all_cards.append(Card(suit,rank))
     
     # This is the length function of the Deck 
     def __len__(self):
-        return len(self.all_card)
+        return len(self.all_cards)
     
     # This does the shuffling of all the deck
     def shuffle_deck(self):
-        random.shuffle(self.all_card)
+        random.shuffle(self.all_cards)
 
     # This is the string function of the deck, to check it.
     def __str__(self):
-        for card in self.all_card:
+        for card in self.all_cards:
             print(card)
         return 'This is the deck you have!'
     
     # This is the function that remove one card 
     # from top of the deck
     def deal_one(self):
-        return self.all_card.pop()
+        return self.all_cards.pop()
+        
+    def add_cards(self,played_cards = []):
+        self.all_cards = played_cards + self.all_cards
+        
+        
         
